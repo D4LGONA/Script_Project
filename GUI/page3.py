44c807a3600cc6cd07c05cc8dade5d6817a0310b
D4LGONA(samsung) <laff3y2297@gmail.com>
2024-05-23 12:01:59 +0900

    리스트박스에 추가 잘 됨 그리고 이미지 자료 업데이트함

--- GUI/page3.py
+++ GUI/page3.py
@@ -3,6 +3,8 @@ import functions
 
 class Page3:
 
+
+
     def update_lb(self):
         self.lb.delete(0, END)  # 기존 결과를 지우고 새로운 검색 결과를 표시합니다.
         for element in functions.bookmark_lists:
@@ -17,12 +19,21 @@ class Page3:
         self.frame1 = Frame(parent_frame, bd=2, relief="solid")
         self.frame1.grid(row=0, column=0, padx=5, pady=10)
 
+        photo = PhotoImage(file="resources/r3.png")
+        resized_photo = photo.subsample(4, 4)
+        label = Label(self.frame1, image=resized_photo)
+        label.photo = resized_photo
+        label.pack(side=LEFT)
+
+        self.frame2 = Frame(parent_frame, bd=2, relief="solid")
+        self.frame2.grid(row=1, column=0, padx=5, pady=10)
+
         # 결과를 출력할 리스트박스 추가
-        self.lb = Listbox(self.frame1, width=70, height=20)
+        self.lb = Listbox(self.frame2, width=70, height=20)
         self.lb.pack(side=LEFT, fill=BOTH, expand=True)
 
         # 스크롤바 추가
-        scrollbar = Scrollbar(self.frame1, orient=VERTICAL)
+        scrollbar = Scrollbar(self.frame2, orient=VERTICAL)
         scrollbar.config(command=self.lb.yview)
         scrollbar.pack(side=RIGHT, fill=Y)
 
